85d40c56f0d14f0f81ae8c4d7572e00c1065673e
Vivekanandhan7677 <125335164+Vivekanandhan7677@users.noreply.github.com>
2026-03-10 14:13:18 +0530

    fixed render port issue

--- app.py
+++ app.py
@@ -466,5 +466,7 @@ def reset_feedback():
 
     return "All Feedback Reset Successfully!"
 
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
